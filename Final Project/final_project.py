b0311898691567a15a283330876155b9edb6df88
Arelys Morales <rlysmorales@gmail.com>
2023-09-27 21:26:42 -0400

    Completed Final Project

--- Final Project/final_project.py
+++ Final Project/final_project.py
@@ -1,76 +1,18 @@
 import os
-import sys
 from hangman_ascii import hangman
 from welcome import welcome, lose, win
-import time
 from wonderwords import RandomWord
 
 
-def user_choice():
-    print(welcome)
-
-    while True:
-        user_input = input("Do you wish to start a new game? Type 'yes' or 'no': ").lower().strip()
-        if user_input == "yes":
-            return True
-
-        elif user_input == "no":
-            clear_screen()
-            sys.exit("Thank You for Playing!!!")
-
-        else:
-            input("Do you wish to start a new game? Type 'yes' or 'no': ").lower().strip()
+def main():
+    hangman_game = Hangman()
+    if hangman_game.user_choice():
+        chosen_word = hangman_game.choose_word()
+        hangman_game.start_game(chosen_word)
 
 
-def choose_word():
-    clear_screen()
-    random_words = RandomWord()
-    chosen_word = random_words.word()
-    return chosen_word
-
-
-def start_game(chosen_word):
-    word_length = len(chosen_word)
-    print(hangman[6])
-    total_lives = 6
-    print(f"You total lives is: {total_lives}")
-
-    display = []
-    for _ in range(word_length):
-        display += "_"
-
-    print(f"WORD DISPLAY: {' '.join(display)}")
-
-    already_guess = []
-    finished_game = False
-    while not finished_game:
-
-        guess = input("Guess a letter: ").lower()
-        clear_screen()
-        if guess in already_guess:
-            print(f"The word: ' {guess} ' has already been used")
-        else:
-            for position in range(word_length):
-                letter = chosen_word[position]
-                if letter == guess:
-                    already_guess.append(guess)
-                    display[position] = letter
-
-        if guess not in chosen_word:
-            print(f"Sorry, the word: ' {guess} ' is not in the random word")
-            total_lives -= 1
-            if total_lives == 0:
-                finished_game = True
-                outcome = "You Lose"
-                display_game_outcome(outcome, chosen_word)
-
-        print(f"WORD DISPLAY: {' '.join(display)}")
-        if "_" not in display:
-            finished_game = True
-            outcome = "You Win"
-            display_game_outcome(outcome, chosen_word)
-
-        print(hangman[total_lives])
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def display_game_outcome(outcome, chosen_word):
@@ -78,25 +20,87 @@ def display_game_outcome(outcome, chosen_word):
         clear_screen()
         print(f" GREAT JOB!! The random word is: ' {chosen_word}'")
         print(win)
-        time.sleep(2)
-        user_choice()
+
+        return "You Win"
 
     elif outcome == "You Lose":
         clear_screen()
         print(f"Sorry but the random word is: ' {chosen_word}'")
         print(lose)
-        time.sleep(2)
-        user_choice()
+        return "You Lose"
 
 
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+class Hangman:
+    def user_choice(self):
+        print(welcome)
 
+        while True:
+            user_input = input("Do you wish to start a new game? Type 'yes' or 'no': ").lower().strip()
+            if user_input == "yes":
+                return True
 
-def main():
-    if user_choice():
-        chosen_word = choose_word()
-        start_game(chosen_word)
+            elif user_input == "no":
+                clear_screen()
+                print("Thank you for Playing!!!")
+                break
+
+            else:
+                input("Do you wish to start a new game? Type 'yes' or 'no': ").lower().strip()
+
+    def choose_word(self):
+        clear_screen()
+        random_words = RandomWord()
+        chosen_word = random_words.word()
+        return chosen_word
+
+    def start_game(self, chosen_word):
+        word_length = len(chosen_word)
+        print(hangman[6])
+        total_lives = 6
+        print(f"You total lives is: {total_lives}")
+
+        display = []
+        for _ in range(word_length):
+            display += "_"
+
+        print(f"WORD DISPLAY: {' '.join(display)}")
+        already_guess = []
+        letter_not_word = []
+        finished_game = False
+        while not finished_game:
+
+            guess = input("Guess a letter: ")
+            clear_screen()
+            print(f"Letters Not in Word: {' '.join(already_guess)}")
+            if guess in already_guess:
+                print(f"The word: ' {guess} ' has already been used")
+            else:
+                for position in range(word_length):
+                    letter = chosen_word[position]
+                    if letter == guess:
+                        already_guess.append(guess)
+                        display[position] = letter
+
+            if guess not in chosen_word:
+                print(f"Sorry, the word: ' {guess} ' is not in the random word")
+                letter_not_word.append(guess)
+                total_lives -= 1
+                if total_lives == 0:
+                    clear_screen()
+                    outcome = "You Lose"
+                    display_game_outcome(outcome, chosen_word)
+                    break
+
+            print(f"WORD DISPLAY: {' '.join(display)}")
+            print(f"Letters Already Used and Not in Word: {' '.join(letter_not_word)}")
+
+            if "_" not in display:
+                clear_screen()
+                outcome = "You Win"
+                display_game_outcome(outcome, chosen_word)
+                break
+
+            print(hangman[total_lives])
 
 
 if __name__ == "__main__":
